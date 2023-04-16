3b255198db713a74b2cacf1615e4fb2fe174a748
Luis Felipe Risch <luis.risch@visagio.com>
2023-04-16 16:49:33 -0300

    feat: finished main

--- t2/main.rb
+++ t2/main.rb
@@ -22,6 +22,10 @@ def tables_name
   models.map(&:constantize).map(&:table_name)
 end
 
+def table_name_to_model(table_name: )
+  models.find { |model| model.constantize.table_name == table_name }
+end
+
 def print_instructions
   puts "Olá!"
   puts "Bem vindo a minha lojinha de compra e venda via console :)"
@@ -50,10 +54,142 @@ def is_command_valid?(command: )
   end
 end
 
+def build_filter(model:, choice:)
+  filter = Hash.new
+
+  model.column_names.each do |column_name|
+    if (index = choice =~ Regexp.new("#{column_name}: ")).present?
+      if(index - 1 < 0 || choice[index - 1] == " ") #gambi
+        part_of_choice = choice[(index + (column_name.size - 1) + 2)..]
+
+        if (content = part_of_choice.match(/"([^"]+)"/)).present?
+          filter[column_name] = content[1]
+        end
+      end
+    end
+  end
+
+  filter
+end
+
+def handle_list(model:)
+  puts "\n\n"
+  puts "Se você quiser listar todos, basta digitar todos"
+  puts 'Se você quiser aplicar um filtro na listagem, basta digitar no seguinte formato: {identificador da coluna}: "{valor}" {identificador da coluna 2}: "{valor 2}" {identificador da coluna 3}: "{valor 3}"'
+  puts 'Por favor, siga estritamente esse formato, só desconsidere "{" "}"'
+
+  choice = (gets&.chomp).to_s
+
+  if choice == 'todos'
+    model.find_each { |instance| instance.introduce_itself }
+  else
+    model.search_with_filter(build_filter(model: model, choice: choice)).find_each { |instance| instance.introduce_itself }
+  end
+end
+
+def handle_insert(model:)
+  puts "\n\n"
+  puts 'Agora, basta digitar os dados no seguinte formato: {identificador da coluna}: "{valor}" {identificador da coluna 2}: "{valor 2}" {identificador da coluna 3}: "{valor 3}"'
+  puts 'Por favor, siga estritamente esse formato, só desconsidere "{" "}"'
+
+  instance = nil
+
+  loop do
+    choice = (gets&.chomp).to_s
+
+    model_attr = build_filter(model: model, choice: choice)
+    model_attr.keys.each { |key| model_attr.delete(key) if model.editable_columns.exclude?(key) }
+
+    instance = model.new(model_attr)
+
+    break if instance.save
+
+    puts "Ocorreu alguns errors de validação..."
+
+    instance.errors.full_messages.each { |error_message| puts "\t- #{error_message}" }
+  end
+
+  instance.introduce_itself
+end
+
+def handle_update(model:)
+  puts "\n\n"
+  puts 'Antes de atualizar, é preciso buscar o(s) registro(s) que você esta querendo atualizar. Ou atualizar todos os registros'
+  puts 'Para atualizar todos os registros, é preciso digitar todos'
+  puts 'Para buscar os registros, digite os dados no seguinte formato: {identificador da coluna}: "{valor}" {identificador da coluna 2}: "{valor 2}" {identificador da coluna 3}: "{valor 3}"'
+  puts 'Por favor, siga estritamente esse formato, só desconsidere "{" "}"'
+
+  loop do
+    choice = (gets&.chomp).to_s
+
+    instances = choice == "todos" ? model.all : model.search_with_filter(build_filter(model: model, choice: choice))
+
+    next if !instances.exists?
+
+    puts 'Pronto, agora insira os dados a serem alterados no seguinte formato: {identificador da coluna}: "{valor}" {identificador da coluna 2}: "{valor 2}" {identificador da coluna 3}: "{valor 3}"'
+    puts 'Por favor, siga estritamente esse formato, só desconsidere "{" "}"'
+
+    loop do
+      choice = (gets&.chomp).to_s
+
+      model_attr = build_filter(model: model, choice: choice)
+      model_attr.keys.each { |key| model_attr.delete(key) if model.editable_columns.exclude?(key) }
+
+      next if model_attr.blank?
+
+      instances.update_all(model_attr)
+      break
+    end
+
+    break
+  end
+end
+
+def handle_exclude(model:)
+  puts "\n\n"
+  puts 'Antes de atualizar, é preciso buscar o(s) registro(s) que você esta querendo atualizar. Ou excluir todos os registros'
+  puts 'Para excluir todos os registros, é preciso digitar todos'
+  puts 'Para buscar os registros, digite os dados no seguinte formato: {identificador da coluna}: "{valor}" {identificador da coluna 2}: "{valor 2}" {identificador da coluna 3}: "{valor 3}"'
+  puts 'Por favor, siga estritamente esse formato, só desconsidere "{" "}"'
+
+  loop do
+    choice = (gets&.chomp).to_s
+
+    instances = choice == "todos" ? model.all : model.search_with_filter(build_filter(model: model, choice: choice))
+
+    next if !instances.exists?
+
+    instances.destroy_all
+
+    break
+  end
+end
+
 def handle_command(splitted_command: )
+  operation = splitted_command.first
+  table_name = splitted_command.last
+
   puts "\n\nEntendido!"
-  puts "Operação escolhida: #{splitted_command.first}"
-  puts "Tabela escolhida: #{splitted_command.last}"
+  puts "Operação escolhida: #{operation}"
+  puts "Tabela escolhida: #{table_name}"
+
+  model = table_name_to_model(table_name: table_name).constantize
+
+  puts "Relembrando esquema da #{model.humanized_table_name}..."
+  model.columns_explanation.each { |k, v| puts " ° #{k}: #{v}" }
+
+  case operation
+  when "lista"
+    handle_list(model: model)
+  when "insere"
+    handle_insert(model: model)
+  when "altera"
+    handle_update(model: model)
+  when "exclui"
+    handle_exclude(model: model)
+  else
+    raise "A combinação da operação: #{operation} com a tabela: #{table} é IMPOSSÍVEL de acontecer! Brigue com o dev responsável por fazer esse script"
+  end
 end
 
 print_instructions
@@ -63,7 +199,7 @@ begin
     puts "\nInsira um comando válido, exemplo: lista users"
     command = (gets&.chomp).to_s
 
-    break if command =~ /exit/
+    break if command == 'exit'
 
     valid, splitted_command = is_command_valid?(command: command)
 
